36534625b265637f4adc06cf57d82706a2fa1dff
Kamil Nguyen Van <nvkga1@gmail.com>
2022-11-19 16:24:01 +0300

    save .fig file

--- src/figma-to-json-index.ts
+++ src/figma-to-json-index.ts
@@ -1,8 +1,17 @@
 import { jsonToFig } from './fig2json'
 import designInJson from './design.json'
+import fs from 'fs'
 
 console.log('starting conversion')
 // console.log(designInJson)
 // console.log(jsonToFig)
-const result = jsonToFig(designInJson)
-console.log(result)
+const f = async () => {
+  const uint8Array: Uint8Array = await jsonToFig(designInJson)
+  const b64encoded = btoa(String.fromCharCode(...uint8Array))
+  const dataStr = "data:application/x-figma;base64," + b64encoded
+
+  fs.writeFileSync("result.fig", dataStr);
+  // console.log(result)
+}
+
+f().then()
